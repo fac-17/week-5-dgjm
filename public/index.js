3f4f17f6acc6135bb8755575ec0cecf0e0d7b979
Colette <colette.rs.shaw@gmail.com>
2019-08-01 17:22:16 +0100

    add comparison exchange rate

--- public/index.js
+++ public/index.js
@@ -7,10 +7,10 @@ search.onclick = function() {
 };
 
 function getQuery(input) {
-    //delete old data
+  //delete old data
 
-    let text = document.getElementById("result-display");
-    text.innerText = `1 GBP today equals...`;
+  let text = document.getElementById("result-display");
+  text.innerText = `1 GBP today equals...`;
 
   let xhr = new XMLHttpRequest();
   let url = `query?=${input}`;
@@ -35,3 +35,26 @@ window.onload = function() {
 };
 
 // document.onload = alert("test");
+
+var oldExchangeRates = {
+  EUR: "1.4372",
+  USD: "1.5558",
+  JPY: "192.7117",
+  AUD: "2.0949",
+  CAD: "2.0139",
+  CHF: "1.4911",
+  SEK: "13.2891",
+  NZD: "2.3473"
+};
+
+search.onclick = function() {
+  let input = document.getElementById("input").value;
+  console.log(input);
+  // document.getElementById("old-rate") = oldExchangeRates[input];
+  document.getElementById("old-rate").innerText =
+    "On 21st July 2015, 1 GBP was " +
+    oldExchangeRates[input] +
+    " " +
+    input +
+    ".";
+};
