f50dd95ab66bfc787b044d595d49f666dc553be7
Victor Masson <victormasson21@gmail.com>
2019-08-01 16:21:59 +0100

    Add countdown

--- public/index.js
+++ public/index.js
@@ -20,11 +20,16 @@ function getQuery(input) {
   xhr.send();
 }
 
-var countdown = "test";
 
-window.onload = function() {
-  console.log("this is working");
-  document.getElementById("deportation").innerText = countdown;
-};
+function updateCountdown () {
+  var deadline = new Date("oct 31, 2019, 23:00:00").getTime()
+  var now = new Date().getTime();
+  var t = deadline - now;
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+  var minutes = Math.floor((t%(1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((t% (1000 * 60)) / 1000);
+  document.getElementById("deportation").innerText = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+}
 
-// document.onload = alert("test");
+setInterval(updateCountdown, 1000);
